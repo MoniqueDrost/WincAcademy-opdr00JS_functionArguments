45c2f46389454761ce0a4f2c41118e7cb66cb5f4
MoniqueDrost <80634091+MoniqueDrost@users.noreply.github.com>
2021-05-06 12:15:01 +0200

    Add files via upload

---
+++ opdr007JS_functionArguments/script.js
@@ -0,0 +1,25 @@
+/*  const paintWall = function(color) {
+    console.log('The wall has been painted ' + color);
+    
+};
+*/
+// paintWall();
+// paintWall('green');
+// paintWall('blue');
+// paintWall();
+
+
+/*  const paintWall = function(location, color) {
+    console.log('The ' + location + ' wall has been painted ' + color);
+    
+};
+
+paintWall('south', 'green');*/
+
+const paintWall = function (color, location) {
+    console.log('The ' + location + ' wall has been painted ' + color);
+
+};
+
+// paintWall('south', 'green');
+paintWall('green', 'south');
